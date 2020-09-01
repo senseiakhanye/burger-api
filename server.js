22643bf791eba63b7f8f407080d739ef728990eb
Katleho Khanye <kakhanye@gmail.com>
2020-09-01 06:02:14 +0200

    Preparing the burger app api

--- server.js
+++ server.js
@@ -1,41 +1,34 @@
-// import express from 'express';
-const express = require('express');
-// const database = require('../database/database.js');
+const app = require('./src/app');
 
-const app = express();
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
+const port = process.env.PORT;
+
+// app.get('/api/burger', (req, res) => {
+//     res.json({data: orders.getAll()});
+//     // res.json([{status: "ok"}]);
 // });
 
-app.use(express.json());
+// app.get('/api/pricelist', (req, res) => {
+//     res.json(prices.getPriceList());
+// });
 
-const port = process.env.PORT || 4007;
+// app.post('/api/burger', (req, res) => {
+//     if (req.body.order == null) {
+//         return res.status(400).json({error: 'request invalid'});
+//     }
+//     // res.status(201).json(orders.addOrder(req.body.order));
+//     res.json({saved : "true"});
+// });
 
-app.get('/api/burger', (req, res) => {
-    // res.json({data: database.readOrders()});
-    res.json([{status: "ok"}]);
-});
+// app.get('/', (req, res) => {
+//     res.status(404).send('You are not authorised');
+// });
 
-app.post('/api/burger', (req, res) => {
-    if (req.body.order == null) {
-        return res.status(400).json({error: 'request invalid'});
-    }
-    // res.status(201).json(database.addOrder(req.body.order));
-    res.json({saved : "true"});
-});
+// app.get('*', (req, res) => {
+//     res.status(404).send('You are not authorised');
+// });
 
-app.get('/', (req, res) => {
-    res.status(404).send('You are not authorised');
-});
-
-app.get('*', (req, res) => {
-    res.status(404).send('You are not authorised');
-});
-
-app.listen( port, () => {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
